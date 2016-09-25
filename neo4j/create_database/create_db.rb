2bd75622d457ac0ce6c3e4868fce1866405600a1
proGuix <guix.default@gmail.com>
2016-09-25 20:12:20 +0200

    first commit

---
+++ neo4j/create_database/create_db.rb
@@ -0,0 +1,23 @@
+#create neo4j database from nodes.csv, relations.csv, types_relation.csv
+
+dir_neo4j = '/home/guillaume/Bureau/neo4j-community-3.0.6/'
+
+puts "BEGIN: NEO4J STOP"
+puts `#{dir_neo4j}bin/neo4j stop`
+puts "END: NEO4J STOP"
+
+puts "BEGIN: DELETE OLD GRAPH.DB"
+puts `rm -r #{dir_neo4j}data/databases/graph.db/`
+puts "END: DELETE OLD GRAPH.DB"
+
+puts "BEGIN: ADD NEW GRAPH.DB"
+puts `#{dir_neo4j}bin/neo4j-import --into graph.db --nodes:Node nodes.csv --nodes:Type_Rel types_relation.csv --relationships relations.csv`
+puts "END: ADD NEW GRAPH.DB"
+
+puts "BEGIN: MOVE GRAPH.DB"
+puts `mv graph.db #{dir_neo4j}data/databases/`
+puts "END: MOVE GRAPH.DB"
+
+puts "BEGIN: ADD CONSTRAINT"
+puts `#{dir_neo4j}bin/neo4j-shell -path #{dir_neo4j}data/databases/graph.db/ -file ../cypher/constraints.cql`
+puts "END: ADD CONSTRAINT"
