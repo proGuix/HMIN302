c8c5b10ade96be2320cf6fe2d2179b4d06d28bca
proGuix <guix.default@gmail.com>
2017-01-21 02:19:02 +0100

    parse with stop_word

--- neo4j/create_database/split_dump_to_csv.rb
+++ neo4j/create_database/split_dump_to_csv.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #split dump.txt to nodes.csv, relations.csv, types_relation.csv
 
 source = open(ARGV[0], 'r')
@@ -5,18 +6,39 @@ nodes_file = open('nodes.csv', 'w')
 relations_file = open('relations.csv', 'w')
 types_relation_file = open('types_relation.csv', 'w')
 
+
+$stop_word = ['erc_INFO-POS-NP','_INFO-POS-MASC','_INFO-NO-MORE-QUESTION','_INFO-POS-PLUR','_INFO-POS-VERB','_INFO-POS-NC','_INFO-POS-FEM','_INFO-POS-SING','_INFO-POS-VER','_INFO-LPOP','_INFO-POS-ADJ','_INFO-LSOUT','_INFO-POS-ADV-EXPR','_INFO-POS-ADV','_POL-POS_PC','_POL-NEUTRE_PC','_POL-NEG_PC','_INFO-WIKI-NO','_INFO-WIKI-YES','_INFO-POS','_POLIT_6FN','_POLIT_1ECO','_POLIT_2PG','_POLIT_5UMP','_POLIT_3PS','_POLIT_4MD','_POLIT_7NOPOL','_INFO','_OCC-IGNORE','_INFO-CNRTL-YES','_INFO-CNRTL-NO','_TERM_GROUP','_SEX_NEUTRAL']
+$stop_eid = []
+
 def valid_line_node(line)
   substr = line[0, 3]
+  line_split = line.force_encoding('iso-8859-1').split('|')
   if substr == 'eid'
-      return true
+    n = line_split[1][3...line_split[1].length-1]
+    for i in 0...$stop_word.length
+      if n == $stop_word[i]
+        eid = line_split[0][4...line_split[0].length].to_i
+        $stop_eid.push(eid)
+        return false
+      end
+    end
+    return true
   end
   return false
 end
 
 def valid_line_relation(line)
   substr = line[0, 3]
+  line_split = line.force_encoding('iso-8859-1').split('|')
   if substr == 'rid'
-      return true
+    n1 = line_split[1][3...line_split[1].length].to_i
+    n2 = line_split[2][3...line_split[2].length].to_i
+    for i in 0...$stop_eid.length
+        if n1 == $stop_eid[i] || n2 == $stop_eid[i]
+          return false
+        end
+    end
+    return true
   end
   return false
 end
